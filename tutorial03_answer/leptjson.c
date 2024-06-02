01476ca97035b571ba3b47ff01faa21bdff4006c
Unicyx <2428250383@qq.com>
2024-06-02 17:36:38 +0800

    03_complete

--- tutorial03_answer/leptjson.c
+++ tutorial03_answer/leptjson.c
@@ -107,7 +107,7 @@ static int lept_parse_string(lept_context* c, lept_value* v) {
                 switch (*p++) {
                     case '\"': PUTC(c, '\"'); break;
                     case '\\': PUTC(c, '\\'); break;
-                    case '/':  PUTC(c, '/' ); break;
+                    case '/':  PUTC(c, '/' ); break;    
                     case 'b':  PUTC(c, '\b'); break;
                     case 'f':  PUTC(c, '\f'); break;
                     case 'n':  PUTC(c, '\n'); break;
